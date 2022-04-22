9d1e852190f773ea94a2e6b39bcb92dcec3a0580
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-04-22 15:11:45 -0400

    1/2 of exercises

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -6,7 +6,16 @@ namespace SchoolPractice
     {
         public static void Main(string[] args)
         {
+            // CLASSES are TYPES of OBJECTS
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            //new below invokes student cronstructor?
+
+            Student student1 = new Student("Sam", 44323, 1, 4.0);
+            Console.WriteLine($"Name: {student1.Name}");
+            Console.WriteLine($"ID {student1.StudentId}");
+            Console.WriteLine($"# Credits: {student1.NumberOfCredits}");
+            Console.WriteLine($"GPA: {student1.Gpa}");
+
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -2,14 +2,33 @@
 
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the Main method in Program
-
+// update the starter code to make use of auto-implemented properties.
 namespace SchoolPractice
 {
+    //4 properties below are declaring all students will have all 4 properties
+
     public class Student
     {
-        private string Name;
-        private int StudentId;
-        private int NumberOfCredits;
-        private double Gpa;
+        // means the property can be get and set!
+        // "Hey, when you want to access the prop, you can do Student.Name ! or you can set it
+        // private property in a claa will only be accessible within this class (in these curly braces!)
+
+
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+
+
+        //constructor ! NEEDED , if not created- it will be an empty one
+        public Student(string name, int studentId, int numberOfCredits, double gpa)
+        {
+            Name = name;
+            StudentId = studentId;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
     }
 }
+
+
