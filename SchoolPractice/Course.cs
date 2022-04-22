4eaf942c395329ba7f2c772b389695da26da244e
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-04-22 15:55:16 -0400

    course Class

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SchoolPractice
+{
+    public class Course
+
+
+    {
+        public string CourseName { get; set; }
+
+        public string ProfessorName { get; set; }
+
+        public List<Student> StudentRoster { get; set; }
+
+
+
+
+
+//constructor ! NEEDED , if not created- it will be an empty one
+
+        public Course(string name, string professorName, List<Student> studentRoster )
+        {
+            CourseName = CourseName;
+            ProfessorName = professorName;
+            StudentRoster = studentRoster;
+        }
+    }
+}
+
+
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace SchoolPractice
 {
@@ -11,10 +13,29 @@ namespace SchoolPractice
             //new below invokes student cronstructor?
 
             Student student1 = new Student("Sam", 44323, 1, 4.0);
+            Student student2 = new Student("Peach", 211, 1, 4.0);
+            Student student3 = new Student("Jackie", 53, 1, 4.0);
+
             Console.WriteLine($"Name: {student1.Name}");
             Console.WriteLine($"ID {student1.StudentId}");
             Console.WriteLine($"# Credits: {student1.NumberOfCredits}");
             Console.WriteLine($"GPA: {student1.Gpa}");
+
+            List<Student> students = new List<Student>();
+            students.Add(student3);
+            students.Add(student1);
+            students.Add(student3);
+
+            Course latin = new Course("Latin 101", "Professor Bourdegard", students);
+
+            foreach (Student student in latin.StudentRoster)
+            {
+                Console.WriteLine($"Name: {student1.Name}");
+                Console.WriteLine($"ID {student1.StudentId}");
+                Console.WriteLine($"# Credits: {student1.NumberOfCredits}");
+                Console.WriteLine($"GPA: {student1.Gpa}");
+
+            }
 
         }
     }
